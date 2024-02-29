22305fbfcba500299ffd428fd211187d4bd3edb8
Academico <Academico@T08KETIPC17A202.instructores.senati.local>
2024-02-29 13:12:00 -0500

    ejercicio0

--- ejercicio0.sql
+++ ejercicio0.sql
@@ -1,13 +1,3 @@
-select * from Products 
-/*
-01. Listar Todos los customers que inicien su id con la 
-letra 'A'. Guardas el archivo como 'ejercicio01.sql' y lo
-subes a tu repositorio
-*/
-
-select * from Customers 
-where CustomerID like 'A%'
-
 /*
 02. Listar a todos los customers que sean de los paises:
 France, Germany, Swedwen. Guardar el archivo como "ejercicio0.sql" y
